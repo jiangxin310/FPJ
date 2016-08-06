2682968555ca6b915e2b7ece7a11201f01d2c72c
wolfben <363273099@qq.com>
2016-08-06 10:08:14 +0800

    增加BaseBLL公共底层逻辑层

---
+++ trunk/FPJ/FPJ.BLL/Default/DefaultBaseBLL.cs
@@ -0,0 +1,16 @@
+﻿using FPJ.DAL.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FPJ.BLL.Default
+{
+    public class DefaultBaseBLL<T> where T : class
+    {
+        /// <summary>
+        /// 基类数据访问DAL
+        /// </summary>
+        protected readonly BaseDAL<T> dal = new BaseDAL<T>();
+    }
+}
